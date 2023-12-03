16ec5b8fccb600f39f7e7d9a05123b2467e3c77f
timtim31 <125752579+timtim31@users.noreply.github.com>
2023-12-03 16:34:37 +0300

    Update 1-1.c

--- 1-1.c
+++ 1-1.c
@@ -6,14 +6,14 @@
  * @param x, y, z значения параметров 
  * @return Значение вычислений    
 */ 
-float function(float x, float y, float z); 
+float getA(float x, float y, float z); 
 
 /** 
  * @brief Функция расчета формулы  
  * @param x, y, z значения параметров  
  * @return Значение вычислений    
 */ 
-float function1(float x, float y, float z); 
+float getB(float x, float y, float z); 
 
 /**
  * @brief Вводимое значение в программу 
@@ -21,20 +21,19 @@ float function1(float x, float y, float z);
 */
 int main ()  
 {  
-    float x, y, z, a, b;  
-    x = 0.61;  
-    y = 0.9;  
-    z = 0.3;  
-    a = function(x, y, z); 
-    b = function1(x, y, z); 
-    printf ("a=%f\n b=%f\n", a, b);  
+    float a, b;  
+    const  float x = 0.61, y = 0.9, z = 0.3; 
+    a = getA(x, y, z); 
+    b = getB(x, y, z); 
+    printf ("a=%f\n b=%f\n", getA(x, y, z), getB(x, y, z));  
+    return 0;
 } 
 
-float function(float x, float y, float z) 
+float getA(float x, float y, float z) 
 { 
     return ((pow(z, 2 * x) + pow (y, -1) * cos(z + y) * x) / (x + 1)); 
 } 
-float function1(float x, float y, float z) 
+float getB(float x, float y, float z) 
 {  
     return (pow(pow (x, 2) + y, 0.5) - pow(y, 2) * pow(sin((x + z) / x), 3)); 
 } 
