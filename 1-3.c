7c86458986bd76499c4c334ffae6d4d629f7add2
timtim31 <125752579+timtim31@users.noreply.github.com>
2023-12-03 16:55:41 +0300

    Update 1-3.c

--- 1-3.c
+++ 1-3.c
@@ -3,11 +3,11 @@
 #include <stdlib.h>
  
 /**
- * @brief Функция расчета формулы
- * @param g, m, f значения параметров
- * @return Значение вычислений  
+ * @brief Функция расчета физической формулы
+ * @param g, m, f значения константы и вводимых параметров
+ * @return Значение вычисления формулы  
 */
-float function(float g, float m, float f);
+float formula(float g, float m, float f);
 
 /**
  * @brief Функция проверки ввода на число 
@@ -17,24 +17,21 @@ float function(float g, float m, float f);
 double fun_scan();
 
 /**
- * @brief Вводимое значение в программу 
+ * @brief Вводимое значение для входа в программу 
  * @return Вывод 0, если программа неверна. Иначе вывод 1
 */
-int main();
-
 int main()
 {
-    float r, m, f, g;
-    m = fun_scan();
-    f = fun_scan();
-    
-    g=6.67*pow(10, -6);
-    r=function(g, m, f);
+    float r;
+    const float g=6.67*pow(10, -6);
+    float m = fun_scan();
+    float f = fun_scan();
+    r=formula(g, m, f);
 
     printf("r=%f\n", r);
 }
 
-float function(float g, float m, float f)
+float formula(float g, float m, float f)
 {
     return (pow(((g*m*m)/f),0.5));
 }
