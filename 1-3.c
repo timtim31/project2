ded677c8ffef960d2869135b909de41bf4d7a2e8
timtim31 <125752579+timtim31@users.noreply.github.com>
2023-12-04 11:19:41 +0300

    Update 1-3.c

--- 1-3.c
+++ 1-3.c
@@ -7,14 +7,14 @@
  * @param g, m, f значения константы и вводимых параметров
  * @return Значение вычисления формулы  
 */
-float formula(float g, float m, float f);
+float formula(float m, float f);
 
 /**
  * @brief Функция проверки ввода на число 
  * @param value значение вводимого параметра 
  * @return Результат проверки   
 */
-double fun_scan();
+double fun_scan(double value);
 
 /**
  * @brief Вводимое значение для входа в программу 
@@ -23,23 +23,24 @@ double fun_scan();
 int main()
 {
     float r;
-    const float g=6.67*pow(10, -6);
     float m = fun_scan();
     float f = fun_scan();
-    r=formula(g, m, f);
+    r=formula(m, f);
     printf("r=%f\n", r);
     return 0;
 }
 
-float formula(float g, float m, float f)
+float formula(float m, float f)
 {
+    const float g=6.67*pow(10, -6);
     return (pow(((g*m*m)/f),0.5));
 }
 
-double fun_scan() {
+double fun_scan(double value) 
+{
   double value;
   int result = scanf("%lf", &value);
-    if (result != 1)
+    if ((result != 1)||(result<0))
     {
       printf("error io");
       abort();
